683833adda6388238fc7b10ee2935b390132c444
Emad Jag <emad@Emads-MacBook-Pro.local>
2016-01-13 13:39:55 +0200

    fix aramex product types label

--- Shopgo/AramexShipping/Model/System/Config/Source/Producttypes.php
+++ Shopgo/AramexShipping/Model/System/Config/Source/Producttypes.php
@@ -6,21 +6,16 @@ use Magento\Framework\Option\ArrayInterface;
 
 class Producttypes implements ArrayInterface
 {
-    const PDE  = "Priority Document Express";
-    const PCE  = "Priority Parcel Express";
-    const PLE  = "Priority Letter Express";
-    const DDE  = "Deferred Document Express";
-    const DPE  = "Deferred Parcel Express";
-    const GDE  = "Ground Document Express";
-    const GPE  = "Ground Parcel Express";
-    const EDX  = "Economy Document Express";
-    const EPX  = "Economy Parcel Express";
+    const PDE  = "PDX";
+    const PCE  = "PCX";
+    const PLE  = "PLX";
+    const DDE  = "DDX";
+    const DPE  = "DPX";
+    const GDE  = "GDX";
+    const GPE  = "GPX";
+    const EDX  = "EDX";
+    const EPX  = "EPX";
 
-    /**
-     * Get positions of lastest news block
-     *
-     * @return array
-     */
     public function toOptionArray()
     {
         return [
