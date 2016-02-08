3087170198b59be51a2075bdc9124a3949bd1549
Emad Jag <Emad Jag>
2016-02-08 12:33:25 +0200

    add city as required checkout filed SG2-284

---
+++ Shopgo/AramexShipping/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/js/model/shipping-rate-processor/new-address':'Shopgo_AramexShipping/js/model/shipping-rate-processor/new-address',
+            'Magento_Checkout/js/model/address-converter':'Shopgo_AramexShipping/js/model/address-converter'
+        }
+    }
+};
